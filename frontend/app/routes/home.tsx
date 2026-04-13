045caf4d90d332c69bb32cd661d3eca0b28c5612
Isiperezz <i.perezr.2023@alumnos.urjc.es>
2026-04-13 11:43:39 +0200

    Component Fyp implementation

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,11 +1,14 @@
 import { Footer } from "../components/footer";
 import type { Route } from "./+types/home";
 import "../app.css";
+import Fyp from "./fyp";
 
 export default function Home() {
   return (
-    <Footer>
-
-    </Footer>
+    <>
+      <Fyp/>
+      <Footer/>
+    </> 
+   
   );
 }
