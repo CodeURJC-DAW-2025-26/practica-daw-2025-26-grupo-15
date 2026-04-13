fc5fdc4c4bf720d8db44e2dabfcf2c3ebdf14c83
huugooocm <cmhugo307@gmail.com>
2026-04-13 12:18:32 +0200

    Add Error component for handling error states and remove unused imports from Home component and

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,9 +1,6 @@
 import { Footer } from "../components/footer";
 import type { Route } from "./+types/home";
 import "../app.css";
-import { Profile } from "./profile";
-import Fyp from "./fyp";
-import { AdminPanel } from "./admin-panel";
 
 export default function Home() {
   return (
